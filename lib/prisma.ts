560cba402412ba66b6f52079097711f519bae519
Gaita <perminusgaita1@gmail.com>
2026-02-22 07:46:11 +0300

    added postgress

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,9 @@
 import { PrismaClient } from '../generated/prisma/client'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { neonConfig } from '@neondatabase/serverless'
-import ws from 'ws'
-
-neonConfig.webSocketConstructor = ws
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL!
-})
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
